c4c315c23d1a2cf21e8f62519fbbc73728317bdb
Stevie-Ray Hartog <stevierayhartog@bluebirdday.nl>
2020-09-15 16:02:54 +0200

    update dependencies

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 global.__rootDirname = path.join(__dirname, 'dist')
 
 const fs = require('fs')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const defaultConfiguration = require('./vue-config/config.default')
 
 const environmentConfigurationPath = `./vue-config/config.${process.env.NODE_ENV}.js`
