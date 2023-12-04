327141d63f0d19268aa07e439c7312dc5bbf961b
Stevie-Ray Hartog <stevierayhartog@bluebirdday.nl>
2023-12-04 23:27:25 +0100

    lint

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    'storybook-dark-mode'
-  ],
+  addons: ['@storybook/addon-essentials', 'storybook-dark-mode'],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
