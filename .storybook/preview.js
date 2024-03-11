825153942472a5df2bdde318bb03e9d97eaf7f10
Stevie-Ray Hartog <mail@stevie-ray.nl>
2024-03-11 22:27:49 +0100

    update dependencies

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,6 @@ setup((app) => {
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
