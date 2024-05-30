7a1a4afc5123890c87779cf6519572810cdec1d4
stevie.hartog <stevie.hartog@gmail.com>
2024-05-30 16:51:46 +0000

    Add idx setup

---
+++ .idx/dev.nix
@@ -0,0 +1,30 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "Vue.volar"
+    "ms-playwright.playwright"
+    "dbaeumer.vscode-eslint"
+    "esbenp.prettier-vscode"
+    "github.vscode-github-actions"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
