bd929b0747d7ab06a286d5baf12050b23e67be29
stevie.hartog <stevie.hartog@gmail.com>
2024-05-31 07:47:45 +0000

    Create globals file

---
+++ src/globals.d.ts
@@ -0,0 +1,47 @@
+// Plugins
+
+declare module '@/plugins/firebase/authentication' {
+  const value: any
+  export default value
+}
+
+declare module '@/plugins/head' {
+  const value: any
+  export default value
+}
+
+declare module '@/plugins/i18n' {
+  const value: any
+  export default value
+}
+
+declare module '@/plugins/pinia' {
+  const value: any
+  export default value
+}
+
+declare module '@/plugins/register-service-worker' {
+  const value: any
+  export default value
+}
+
+declare module '@/plugins/vuetify' {
+  const value: any
+  export default value
+}
+
+// Helpers
+declare module '@/helpers/countries' {
+  const value: any
+  export default value
+}
+
+declare module '@/helpers/hangboards' {
+  const value: any
+  export default value
+}
+
+declare module '@/helpers/online' {
+  const value: any
+  export default value
+}
