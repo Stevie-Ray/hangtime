62e04eb060147776d74422f24057b76b4df18d5a
stevie.hartog <stevie.hartog@gmail.com>
2024-06-01 12:53:50 +0000

    816

--- src/components/atoms/ExerciseHangboard/ExerciseHangboard.vue
+++ src/components/atoms/ExerciseHangboard/ExerciseHangboard.vue
@@ -2,15 +2,16 @@
 import { computed } from 'vue'
 import InlineSvg from 'vue-inline-svg'
 import { useUser } from '@/stores/user'
+import { Exercise } from '@/interfaces/workouts.interface'
 
 const { getHangboardByIds } = useUser()
 
 const props = defineProps({
   exercise: {
-    type: Object
+    type: Object as () => Exercise
   },
   hangboard: {
-    type: Object,
+    type: Object as () => { hangboard: number; company: number },
     required: true
   },
   edit: {
@@ -38,10 +39,10 @@ const hangboardImage = computed(() => {
     : ''
 })
 
-const classHold = (hand) =>
+const classHold = (hand: "left" | "right") =>
   props.exercise && props.exercise[hand] !== null ? `h${props.exercise[hand] + 1}` : null
 
-const toggleHold = (hand, e) => {
+const toggleHold = (hand: "left" | "right", e: Event) => {
   if (props.edit) {
     const hold = e.target.id
     const n = hold.startsWith('h')
@@ -53,9 +54,9 @@ const toggleHold = (hand, e) => {
 }
 
 const nextImage = () => {
-  if (props.exercise.rotate === undefined) {
+  if (props.exercise?.rotate === undefined) {
     emit('rotate', 1)
-  } else if (props.exercise.rotate + 1 !== getHangboard.value.sides.length) {
+  } else if (props.exercise.rotate + 1 !== getHangboard.value?.sides?.length) {
     emit('rotate', props.exercise.rotate + 1)
   } else {
     emit('rotate', 0)
--- src/interfaces/workouts.interface.ts
+++ src/interfaces/workouts.interface.ts
@@ -1,57 +1,77 @@
-export interface Excercise {
+/**
+ * Represents an exercise in a hangboard workout.
+ */
+export interface Exercise {
+  /** Number representing the exercise. */
   exercise: number;
+  /** Number representing the grip used in the exercise. */
   grip: number;
+  /** Difficulty level of the exercise. */
   level: number;
-  left: number;
-  right: number;
+  /** Number of repetitions for the left side. */
+  left: number | null;
+  /** Number of repetitions for the right side. */
+  right: number | null;
+  /** Rotate */
+  rotate?: number;
+  /** Pause duration between repetitions. */
   pause: number;
+  /** Duration of the hold. */
   hold: number;
+  /** Number of pull-ups performed during the exercise. */
   pullups: number;
+  /** Number of repetitions. */
   repeat: number;
+  /** Rest duration between sets. */
   rest: number;
+  /** Additional weight used in the exercise (if any). */
   weight: number;
+  /** Optional: Duration of the exercise in seconds. */
   time?: number;
-  max?: boolean
+  /** Optional: Indicates if it's the maximum effort exercise. */
+  max?: boolean;
+  /** Notes or additional information about the exercise. */
   notes: string;
 }
+
 /**
- * Represents a hangboard workout
+ * Represents a hangboard workout.
  */
 export interface Workout {
-  /** Unique identifier for the workout (optional) */
+  /** Unique identifier for the workout (optional). */
   id?: string;
-  /** Name of the workout */
+  /** Name of the workout. */
   name: string;
-  /** Description of the workout */
+  /** Description of the workout. */
   description: string;
-  /** Difficulty level of the workout */
+  /** Difficulty level of the workout. */
   level: number;
-  /** The hangboard used in the workout */
+  /** The hangboard used in the workout. */
   hangboard: string;
-  /** Company that created the workout */
+  /** Company that created the workout. */
   company: string;
-  /** List of exercises included in the workout */
-  exercises: Excercise[];
-  /** Duration of the workout in minutes */
+  /** List of exercises included in the workout. */
+  exercises: Exercise[];
+  /** Duration of the workout in minutes. */
   time: number;
-  /** Indicates if the workout is shared publicly */
+  /** Indicates if the workout is shared publicly. */
   share: boolean;
-  /** URL to a video demonstrating the workout */
+  /** URL to a video demonstrating the workout. */
   video: string;
-  /** List of subscriber IDs who follow the workout */
+  /** List of subscriber IDs who follow the workout. */
   subscribers: Array<string>;
-  /** User information of the workout creator */
+  /** User information of the workout creator. */
   user: {
-    /** Display name of the user */
+    /** Display name of the user. */
     displayName: string;
-    /** Climbing grade of the user */
+    /** Climbing grade of the user. */
     grade: number;
-    /** Unique identifier for the user */
+    /** Unique identifier for the user. */
     id: string;
-    /** URL to the user's profile photo */
+    /** URL to the user's profile photo. */
     photoURL: string;
   };
-  /** Timestamp of the last update (optional) */
+  /** Timestamp of the last update (optional). */
   updateTimestamp?: any;
 }
 
@@ -59,8 +79,8 @@ export interface Workout {
  * Represents a leaderboard for workouts.
  */
 export interface Leaderboard {
-  /** Rank of the leaderboard entry */
+  /** Rank of the leaderboard entry. */
   rank: string;
-  /** List of entries in the leaderboard */
+  /** List of entries in the leaderboard. */
   leaderboard: Array<any>;
 }
