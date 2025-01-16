a921b8e7d0dc5107c6b3a1e2e352257edd4276fa
Stevie-Ray Hartog <mail@stevie-ray.nl>
2025-01-16 21:54:20 +0100

    add cloud message composable (+iOS)

--- src/components/molecules/NotificationBell/NotificationBell.vue
+++ src/components/molecules/NotificationBell/NotificationBell.vue
@@ -1,84 +1,26 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { storeToRefs } from 'pinia'
-import { usePermission, useSupported } from '@vueuse/core'
-import { getMessaging, getToken, deleteToken } from 'firebase/messaging'
-import firebaseApp from '@/plugins/firebase'
-
-import { useAuthenticationStore } from '@/stores/authentication.store'
-const { updateUser } = useAuthenticationStore()
-
-// const { isSupported } = useWebNotification()
-
-const isSupported = useSupported(() => {
-  if (!window || !('Notification' in window)) return false
-  return true
-})
+import { useFirebaseCloudMessaging } from '@/composables/useFirebaseCloudMessaging'
+import { usePermission } from '@vueuse/core'
 
 const notificationAccess = usePermission('notifications')
 
-const { user } = storeToRefs(useAuthenticationStore())
-
-const currentToken = ref<string | undefined>(undefined)
-const messaging = getMessaging(firebaseApp)
-const vapidKey =
-  'BLYKE2FC7Wjeyz_6m_wOc7rLUdRf4XGmJUhoIBKS4pfxaA8GP5dT5XCOkqCdTiayKlv_U-WwiDmkVPOlqHgowPE'
-
-const requestPermission = async () => {
-  try {
-    // we just need to check if the permission is granted
-    let permissionResult = notificationAccess.value as NotificationPermission
-    console.log(permissionResult)
-    if (permissionResult !== 'granted') {
-      permissionResult = await Notification.requestPermission()
-      console.log(permissionResult)
-    }
-    if (permissionResult === 'granted') {
-      // Get FCM token
-      const token = await getToken(messaging, { vapidKey })
-      if (token) {
-        currentToken.value = token
-        // Save token to user settings
-        if (user.value && !user.value.settings.token) {
-          user.value.settings.token = currentToken.value
-          updateUser()
-        }
-      }
-    } else {
-      console.warn('Notification permission not granted.')
-    }
-  } catch (err) {
-    console.error('Error requesting FCM permission:', err)
-  }
-}
-
-const removeToken = async () => {
-  try {
-    if (currentToken.value) {
-      await deleteToken(messaging)
-      currentToken.value = undefined
-      // Remove token from user
-      if (user.value && user.value.settings.token) {
-        user.value.settings.token = currentToken.value
-        updateUser()
-      }
-    }
-  } catch (err) {
-    console.error('Error deleting FCM token:', err)
-  }
-}
-
-onMounted(() => {
-  if (user.value && user.value.settings.token) {
-    currentToken.value = user.value.settings.token
-  }
-})
+const {
+  iOSPushCapability,
+  iOSNotificationAccess,
+  currentToken,
+  removeToken,
+  requestPermission,
+  pushCapability
+} = useFirebaseCloudMessaging()
 </script>
 
 <template>
-  <div v-if="isSupported">
+  <div v-if="pushCapability || iOSPushCapability">
     <v-btn
-      v-if="notificationAccess !== 'granted'"
+      v-if="
+        (pushCapability && notificationAccess !== 'granted') ||
+        (iOSPushCapability && iOSNotificationAccess !== 'granted')
+      "
       icon
       @click="requestPermission"
       title="Enable global notifications"
---
+++ src/composables/useFirebaseCloudMessaging.ts
@@ -0,0 +1,226 @@
+import { storeToRefs } from 'pinia'
+import { ref, onMounted } from 'vue'
+import { usePermission, useSupported } from '@vueuse/core'
+import { getMessaging, getToken, deleteToken } from 'firebase/messaging'
+import firebaseApp from '@/plugins/firebase'
+const { updateUser } = useAuthenticationStore()
+import { useAuthenticationStore } from '@/stores/authentication.store'
+
+/**
+ * Declare global window interface for iOS native app
+ */
+declare global {
+  interface Window {
+    webkit?: {
+      messageHandlers?: Record<string, { postMessage: (msg: unknown) => void }>
+    }
+  }
+}
+
+export function useFirebaseCloudMessaging() {
+  const iOSPushCapability = ref(false)
+  const iOSNotificationAccess = ref<PermissionState>('prompt')
+
+  const { user } = storeToRefs(useAuthenticationStore())
+
+  const notificationAccess = usePermission('notifications')
+
+  const currentToken = ref<string | undefined>(undefined)
+  const messaging = getMessaging(firebaseApp)
+  const vapidKey =
+    'BLYKE2FC7Wjeyz_6m_wOc7rLUdRf4XGmJUhoIBKS4pfxaA8GP5dT5XCOkqCdTiayKlv_U-WwiDmkVPOlqHgowPE'
+
+  const pushCapability = useSupported(() => {
+    if (!window || !('Notification' in window)) return false
+    return true
+  })
+
+  const requestPermission = async () => {
+    // iOS native app
+    if (iOSPushCapability.value) {
+      pushPermissionRequest()
+      // we can not await the event listener, so we directly request the token
+      pushTokenRequest()
+    } else {
+      // web browser
+      try {
+        // we just need to check if the permission is granted
+        let permissionResult = notificationAccess.value as NotificationPermission
+        console.log(permissionResult)
+        if (permissionResult !== 'granted') {
+          permissionResult = await Notification.requestPermission()
+        }
+        if (permissionResult === 'granted') {
+          // Get FCM token
+          const token = await getToken(messaging, { vapidKey })
+          if (token) {
+            currentToken.value = token
+            // Save token to user settings
+            if (user.value && !user.value.settings.token) {
+              user.value.settings.token = currentToken.value
+              updateUser()
+            }
+          }
+        } else {
+          console.warn('Notification permission not granted.')
+        }
+      } catch (err) {
+        console.error('Error requesting FCM permission:', err)
+      }
+    }
+  }
+
+  const removeToken = async () => {
+    try {
+      if (currentToken.value) {
+        await deleteToken(messaging)
+        currentToken.value = undefined
+        // Remove token from user
+        if (user.value && user.value.settings.token) {
+          delete user.value.settings.token
+          updateUser()
+        }
+      }
+    } catch (err) {
+      console.error('Error deleting FCM token:', err)
+    }
+  }
+
+  onMounted(() => {
+    if (user.value && user.value.settings.token) {
+      currentToken.value = user.value.settings.token
+    }
+
+    if (
+      window.webkit &&
+      window.webkit.messageHandlers?.['push-permission-request'] &&
+      window.webkit.messageHandlers?.['push-permission-state']
+    ) {
+      iOSPushCapability.value = true
+    }
+
+    pushPermissionState()
+
+    // Listen for push-permission-request
+    window.addEventListener('push-permission-request', (event: Event) => {
+      const customEvent = event as CustomEvent
+      if (customEvent?.detail) {
+        switch (customEvent.detail) {
+          case 'granted':
+            iOSNotificationAccess.value = 'granted'
+            break
+          default:
+            iOSNotificationAccess.value = 'denied'
+            break
+        }
+      }
+    })
+
+    // Listen for push-permission-state
+    window.addEventListener('push-permission-state', (event: Event) => {
+      const customEvent = event as CustomEvent
+      if (customEvent?.detail) {
+        switch (customEvent.detail) {
+          case 'notDetermined':
+            // permission not asked
+            iOSNotificationAccess.value = 'prompt'
+            break
+          case 'denied':
+            // permission denied
+            iOSNotificationAccess.value = 'denied'
+            break
+          case 'authorized':
+          case 'ephemeral':
+          case 'provisional':
+            // permission granted
+            iOSNotificationAccess.value = 'granted'
+            break
+          case 'unknown':
+          default:
+            // something wrong
+            console.error(customEvent.detail)
+            break
+        }
+      }
+    })
+
+    // Listen for push-notification
+    window.addEventListener('push-notification', (event: Event) => {
+      const customEvent = event as CustomEvent
+      if (customEvent?.detail) {
+        alert(customEvent.detail)
+      }
+    })
+
+    // Listen for push-token
+    window.addEventListener('push-token', async (event: Event) => {
+      const customEvent = event as CustomEvent
+      if (customEvent?.detail) {
+        currentToken.value = customEvent.detail
+        if (user.value && !user.value.settings.token) {
+          user.value.settings.token = currentToken.value
+          updateUser()
+        }
+      }
+    })
+  })
+
+  /**
+   * Request native push permission from iOS app
+   */
+  function pushPermissionRequest() {
+    if (iOSPushCapability.value) {
+      window.webkit?.messageHandlers?.['push-permission-request']?.postMessage(
+        'push-permission-request'
+      )
+    }
+  }
+
+  /**
+   * Request native push permission state from iOS app
+   */
+  function pushPermissionState() {
+    if (iOSPushCapability.value) {
+      window.webkit?.messageHandlers?.['push-permission-state']?.postMessage(
+        'push-permission-state'
+      )
+    }
+  }
+
+  /**
+   * Subscribe to a topic using iOS native app
+   * @param topic - The topic to subscribe to
+   * @param eventValue - The value to subscribe to
+   * @param unsubscribe - Whether to unsubscribe from the topic
+   */
+
+  function pushSubscribeTopic(topic: string, eventValue: unknown, unsubscribe?: boolean) {
+    if (iOSPushCapability.value) {
+      window.webkit?.messageHandlers?.['push-subscribe']?.postMessage(
+        JSON.stringify({ topic, eventValue, unsubscribe })
+      )
+    }
+  }
+
+  /**
+   * Push token request to iOS native app
+   */
+  function pushTokenRequest() {
+    if (iOSPushCapability.value) {
+      window.webkit?.messageHandlers?.['push-token']?.postMessage('push-token')
+    }
+  }
+
+  return {
+    pushCapability,
+    currentToken,
+    iOSPushCapability,
+    iOSNotificationAccess,
+    requestPermission,
+    removeToken,
+    pushPermissionRequest,
+    pushPermissionState,
+    pushSubscribeTopic,
+    pushTokenRequest
+  }
+}
